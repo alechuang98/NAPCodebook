9da7dc212913453f0a9a9f0ba6feffd2a3604dac
kimnai862 <37082020+kimnai862@users.noreply.github.com>
2018-09-20 16:09:36 +0800

    Update default.cpp

--- codes/Basic/default.cpp
+++ codes/Basic/default.cpp
@@ -5,7 +5,6 @@
 #define MP make_pair
 #define ALL(x) begin(x),end(x)
 #define SZ(x) ((int)(x).size())
-#define FZ(n) memset((n),0,sizeof(n))
 using namespace std;
 typedef long long LL;
 typedef double D;
@@ -14,7 +13,7 @@ typedef pair<int, int> PII;
 typedef pair<LL, LL> PLL;
 #define rep(i, n) for(int i = 0; i < n; i ++)
 #define rep1(i, a, b) for(int i = a; i < b; i ++)
-#define per(i, a, b) for(int i = a; i >= b; i --)
+#define per1(i, a, b) for(int i = a; i >= b; i --)
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0)
 
 int main(){
