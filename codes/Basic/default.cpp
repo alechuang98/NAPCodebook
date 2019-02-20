36ed21fedf4bbe1e1a0bfeff0d6a0c0e30f65156
alechuang98 <alechuang98@gmail.com>
2019-02-20 16:54:24 +0800

    heavy_decompo_TBA

--- codes/Basic/default.cpp
+++ codes/Basic/default.cpp
@@ -1,21 +1,20 @@
+// #pragma GCC optimize ("-O2")
+// #pragma GCC optimize ("unroll-loops")
 #include<bits/stdc++.h>
+using namespace std;
 #define F first
 #define S second
 #define pb push_back
 #define MP make_pair
 #define ALL(x) begin(x),end(x)
 #define SZ(x) ((int)(x).size())
-using namespace std;
 typedef long long LL;
-typedef double D;
+typedef double DB;
 typedef long double LDB;
 typedef pair<int, int> PII;
 typedef pair<LL, LL> PLL;
-#define rep(i, n) for(int i = 0; i < n; i ++)
-#define rep1(i, a, b) for(int i = a; i < b; i ++)
-#define per1(i, a, b) for(int i = a; i >= b; i --)
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0)
-
+// #define IOS ios_base::sync_with_stdio(0); cin.tie(0)
+const int MXN = (int)1e6 + 7;
 int main(){
-	
+	return 0;
 }
--- codes/Basic/random.cpp
+++ codes/Basic/random.cpp
@@ -1,5 +1,6 @@
 #include <random>
 mt19937 rng(0x5EED);
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int randint(int lb, int ub){ 
   return uniform_int_distribution<int>(lb, ub)(rng); 
 }
